66b40e40f1b989e8f5921490ea9a92fd05405b20
savionasc <savionasc@gmail.com>
2020-07-16 10:23:35 -0300

    Icone ao site
    #release

--- Release/index.php
+++ Release/index.php
@@ -18,11 +18,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="@savionasc">
+    <meta name="author" content="Sávio Nascimento">
     <!-- <link rel="icon" href="favicon.ico"> -->
 
     <title>Análises COVID-19</title>
+    <link rel="icon" href="./covid19-icone.png">
 
     <!-- Bootstrap core CSS -->
     <link href="./bootstrap/bootstrap.min.css" rel="stylesheet">
